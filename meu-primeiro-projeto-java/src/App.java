e3d4219777a961a1ddaa7d161299a75f8d8ac0a2
Caroline <caroline.sousa24@otlook.com>
2022-11-09 20:27:24 -0300

    Meu primeiro projeto java

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá amigos da DIO");
+        System.out.println("Olá amigos da DIO!");
     }
 }
